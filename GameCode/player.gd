1ee038d40ef42df4655937ba01e98975d4b57d61
Brandon McClure <brandonmcclure89@gmail.com>
2024-04-13 14:39:23 -0600

    mouse click will move player to a location

--- GameCode/player.gd
+++ GameCode/player.gd
@@ -4,25 +4,42 @@ signal hit
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
-
-
+var velocity
+var target_destination = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	velocity = Vector2.ZERO
 	hide()
 
-
+func _input(event):
+# Mouse in viewport coordinates.
+	if not event is InputEventMouseButton:
+		return
+	#print("Mouse Click/Unclick at: ", event.position)
+	target_destination = event.position
+	print('set the dest: ', event.position)
+	#elif event is InputEventMouseMotion:
+		#print("Mouse Motion at: ", event.position)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+	if target_destination != null:
+		position = position.move_toward(target_destination,delta*speed)
+		
+	velocity = Vector2.ZERO
+	 # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
+		target_destination = null
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
+		target_destination = null
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
+		target_destination = null
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
+		target_destination = null
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
